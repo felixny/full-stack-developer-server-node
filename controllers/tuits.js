e6c0c4645a7186d2355f9ebb1448fa8c58268b68
Felix <nampanya.f@northeastern.edu>
2022-03-30 11:35:52 -0400

    a8 node.js sp22

---
+++ controllers/tuits.js
@@ -0,0 +1,18 @@
+let tuits = [
+    {
+        "_id": "123",
+        "postedBy": { "username": "NASA" },
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345,
+        "dislikes": 10,
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785,
+        "dislikes": 4678,
+    }
+]
+
+export default tuits;
